d4c1c3da227765769447e381c3164b245497a995
Adam Cox <mr.adamcox@gmail.com>
2023-12-30 14:15:15 -0600

    update to dev/7.2.x

--- presutah/webpack/webpack-node-modules-aliases.js
+++ presutah/webpack/webpack-node-modules-aliases.js
@@ -1,5 +1,5 @@
 module.exports = {
     PROJECT_NODE_MODULES_ALIASES: JSON.stringify({
-        // "print-css": "Path.resolve(__dirname, `${APP_ROOT}/media/css/print.css`)",
+        // "example-node-module": "Path.resolve(__dirname, `${APP_ROOT}/media/node_modules/example-node-module`)",
     }),
 };
